aac5daf3dc556cbccec3d57d7f1e29e0d55a60d5
Kerwin Jorbina <kerwin.jorbina@gmail.com>
2017-03-05 20:46:16 +0200

    commit all

---
+++ src/main/java/com/predictiveprocess/PredictiveBackendApplication.java
@@ -0,0 +1,12 @@
+package com.predictiveprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredictiveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredictiveBackendApplication.class, args);
+	}
+}
